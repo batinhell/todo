4c6a8880ab438a545d7402c14ed152a5f45bb6d0
batinhell <batinhell@gmail.com>
2016-10-23 20:51:49 +0300

    Init.

---
+++ todo.controller.js
@@ -0,0 +1,43 @@
+(function () {
+
+    angular.module('todo').
+    controller('TodoController', TodoController);
+
+    function TodoController () {
+        var vm = this;
+        vm.new = '';
+        vm.add = add;
+        vm.remaining = remaining;
+        vm.edit = edit;
+        vm.save = save;
+        vm.revert = revert;
+
+        vm.todos = [
+            {text: 'do a barell roll', done: true, editing: false},
+            {text: 'buy a boat', done: false, editing: false},
+        ];
+
+        function add() {
+            vm.todos.push({text: vm.new, done: false, editing: false});
+            vm.new = '';
+        }
+
+        function remaining() {
+            return vm.todos.filter(function (todo) {
+                return todo.done;
+            }).length;
+        }
+
+        function edit(todo) {
+            todo.editing = true;
+            vm.originalTodo = angular.extend({}, todo);
+        }
+
+        function save(todo) {
+            todo.editing = false;
+            todo.done = false;
+        }
+
+    }
+
+}());
